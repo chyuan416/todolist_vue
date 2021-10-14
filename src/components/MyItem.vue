c0c6b979d80b0f2b743527b0e9520fccd71f6422
chyuan416 <chyuanhan@gmail.com>
2021-10-14 15:25:19 +0800

    提交

---
+++ src/components/MyItem.vue
@@ -0,0 +1,120 @@
+<template>
+  <transition name="todo" appear>
+    <li>
+      <label>
+        <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)"/>
+        <!--如下代碼也能實現功能，但不太推薦，因為修改了props，違反了原則-->
+        <!--<input type="checkbox" v-model="todo.done"/>-->
+        <span v-show="!todo.isEdit">{{todo.title}}</span>
+        <input 
+          type="text" 
+          v-show="todo.isEdit" 
+          :value="todo.title" 
+          @blur="handleBlur(todo,$event)"
+          ref="inputTitle"
+        >
+      </label>
+      <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
+      <button class="btn btn-edit" v-show="!todo.isEdit" @click="handleEdit(todo)">編輯</button>
+    </li>
+  </transition>
+</template>
+
+<script>
+import pubsub from 'pubsub-js'
+  export default {
+    name: 'MyItem',
+    props:['todo'],
+    methods: {
+      //勾選or取消勾選
+      handleCheck(id){
+        //通知App組件將對應的todo對象的done值取反
+        this.$bus.$emit('checkTodo',id)
+      },
+      handleDelete(id){
+        if(confirm('確定刪除嗎？')){
+          //this.$bus.$emit('deleteTodo',id)
+          pubsub.publish('deleteTodo',id)
+        }
+      },
+      handleEdit(todo){
+        
+        if(Object.prototype.hasOwnProperty.call(todo,'isEdit')){
+          todo.isEdit = true
+        }else{
+          this.$set(todo,'isEdit',true)
+        }
+        this.$nextTick(function(){
+          this.$refs.inputTitle.focus();
+        })
+      },
+      handleBlur(todo,event){
+        todo.isEdit = false
+        if(!event.target.value.trim()) return alert('輸入不能為空')
+        this.$bus.$emit('updateTodo',todo.id,event.target.value)
+      }
+    }
+  }
+</script>
+
+<style scoped>  
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+li:hover{
+  background-color: #ddd;
+}
+
+li:hover button{
+  display: block;
+}
+
+.todo-enter-active{
+  animation: atguigu 0.5s linear;
+}
+
+.todo-leave-active{
+  animation: atguigu 0.5s linear reverse;
+}
+
+@keyframes atguigu {
+  from{
+    transform: translateX(100%);
+  }
+  to{
+    transform: translateX(0px);
+  }
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pages: {
+    index: {
+      //入口
+      entry: 'src/main.js',
+    },
+  },
+	lintOnSave:false //关闭语法检查
+
+}
